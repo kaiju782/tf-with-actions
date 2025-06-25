b3ef5e91072a8d0198bc950d2143aff0cb998548
kaiju782 <didwjdah7@sunmoon.ac.kr>
2025-06-25 15:29:24 +0900

    update cloud

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ resource "aws_vpc" "backend-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "tf-backend-plan"
+    Name = "tf-backend-cloud"
   }
+}
+
+terraform { 
+  cloud { 
+    organization = "tfc-devops-yjm"
+
+    workspaces { 
+      name = "tfc-devops"
+    } 
+  } 
 }
--- provider.tf
+++ provider.tf
@@ -5,12 +5,7 @@ terraform {
       version = ">= 5.0.0"
     }
   }
-  backend "s3" {
-    bucket         = "bucket-test-yjm2222"
-    key            = "terraform/state-test/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock"
-  }
+  # backend "s3" 블록 삭제됨!
 }
 
 provider "aws" {
